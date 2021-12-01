4ef4b6608abb466dc8f2797260ad66fde9f72660
Bill Zack <bzak954@gmail.com>
2021-12-01 20:10:50 +0000

    add navigator

--- src/Navigators/stackNavigator.js
+++ src/Navigators/stackNavigator.js
@@ -2,13 +2,26 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import ReviewDetailsPage from "../Pages/reviewDetailsPage";
+import Main from "../Pages/mainPage";
 
 const Stack = createStackNavigator();
 
 function MyStack() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: "red",
+          tabBarStyle: [
+            {
+              display: "flex",
+            },
+            null,
+          ],
+        }}
+      >
+        <Stack.Screen name="Main" component={Main} />
         <Stack.Screen name="ReviewDetails" component={ReviewDetailsPage} />
       </Stack.Navigator>
     </NavigationContainer>
