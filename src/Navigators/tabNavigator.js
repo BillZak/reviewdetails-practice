3d4e1f28557105ab390bb87243b971365bfe0f57
Bill Zack <bzak954@gmail.com>
2021-12-01 20:11:11 +0000

    change route to app

--- src/Navigators/tabNavigator.js
+++ src/Navigators/tabNavigator.js
@@ -13,77 +13,75 @@ const Tab = createBottomTabNavigator();
 
 function TabNavigator() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          headerShown: false,
-          tabBarActiveTintColor: "red",
-          tabBarStyle: [
-            {
-              display: "flex",
-            },
-            null,
-          ],
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "red",
+        tabBarStyle: [
+          {
+            display: "flex",
+          },
+          null,
+        ],
+      }}
+    >
+      <Tab.Screen
+        name="home"
+        component={HomePage}
+        options={{
+          tabBarIcon: (props) => (
+            <Ionicons
+              name="home-outline"
+              size={props.size}
+              color={props.color}
+            />
+          ),
+          tabBarLabel: () => {
+            return null;
+          },
         }}
-      >
-        <Tab.Screen
-          name="home"
-          component={HomePage}
-          options={{
-            tabBarIcon: (props) => (
-              <Ionicons
-                name="home-outline"
-                size={props.size}
-                color={props.color}
-              />
-            ),
-            tabBarLabel: () => {
-              return null;
-            },
-          }}
-        />
-        <Tab.Screen
-          name="search"
-          component={SearchPage}
-          options={{
-            tabBarIcon: (props) => (
-              <Ionicons name="search" size={props.size} color={props.color} />
-            ),
-            tabBarLabel: () => {
-              return null;
-            },
-          }}
-        />
-        <Tab.Screen
-          name="notification"
-          component={NotificationPage}
-          options={{
-            tabBarIcon: (props) => (
-              <Ionicons
-                name="notifications-outline"
-                size={props.size}
-                color={props.color}
-              />
-            ),
-            tabBarLabel: () => {
-              return null;
-            },
-          }}
-        />
-        <Tab.Screen
-          name="aboutme"
-          component={AboutmePage}
-          options={{
-            tabBarIcon: (props) => (
-              <AntDesign name="user" size={props.size} color={props.color} />
-            ),
-            tabBarLabel: () => {
-              return null;
-            },
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+      />
+      <Tab.Screen
+        name="search"
+        component={SearchPage}
+        options={{
+          tabBarIcon: (props) => (
+            <Ionicons name="search" size={props.size} color={props.color} />
+          ),
+          tabBarLabel: () => {
+            return null;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="notification"
+        component={NotificationPage}
+        options={{
+          tabBarIcon: (props) => (
+            <Ionicons
+              name="notifications-outline"
+              size={props.size}
+              color={props.color}
+            />
+          ),
+          tabBarLabel: () => {
+            return null;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="aboutme"
+        component={AboutmePage}
+        options={{
+          tabBarIcon: (props) => (
+            <AntDesign name="user" size={props.size} color={props.color} />
+          ),
+          tabBarLabel: () => {
+            return null;
+          },
+        }}
+      />
+    </Tab.Navigator>
   );
 }
 
